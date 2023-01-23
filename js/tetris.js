c8dac851b803f24f1840f8967952e12c4a0484cd
IMJOne <mompapa7@gmail.com>
2023-01-23 18:36:31 +0900

    Init game

--- js/tetris.js
+++ js/tetris.js
@@ -31,3 +31,46 @@ const movingItem = {
 
 // Prevent context
 document.addEventListener('contextmenu', e => e.preventDefault());
+
+// Game start
+startBtn.addEventListener('click', () => {
+  init();
+});
+document.addEventListener('keydown', e => {
+  if (startScreen.style.display === 'none') return;
+  if (e.key === 'Enter') {
+    init();
+  }
+  return;
+});
+
+// Functions
+function init() {
+  started = true;
+  score = 0;
+
+  startScreen.style.display = 'none';
+  gameField.style.display = 'block';
+  gameScore.innerText = score;
+  updateTimer();
+  startTimer();
+
+  // 블럭 잠시 담아두기
+  // 기존의 블럭에는 영향이 가지 않도록 spread 연산자를 통해 새로운 블럭 생성
+  tempMovingItem = { ...movingItem };
+  for (let i = 0; i < GAME_ROWS; i++) {
+    prependNewLine();
+  }
+  generateNewBlock();
+}
+
+function prependNewLine() {
+  const tr = document.createElement('tr');
+  const td = document.createElement('td');
+  for (let j = 0; j < GAME_COLS; j++) {
+    const matrix = document.createElement('td');
+    tr.prepend(matrix);
+  }
+  td.prepend(tr);
+  tetrisBoard.prepend(tr);
+}
