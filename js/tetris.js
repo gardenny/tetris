a4100009aca21ed9148792138d7e6bc989b3c5f0
IMJOne <mompapa7@gmail.com>
2023-01-23 18:37:15 +0900

    Generate new block

--- js/tetris.js
+++ js/tetris.js
@@ -74,3 +74,56 @@ function prependNewLine() {
   td.prepend(tr);
   tetrisBoard.prepend(tr);
 }
+
+function generateNewBlock() {
+  stopDescendBlock();
+  descendBlock();
+
+  const blockArray = Object.entries(BLOCKS);
+  const randomIndex = Math.floor(Math.random() * blockArray.length);
+
+  movingItem.type = blockArray[randomIndex][0]; // 랜덤한 모양의 블럭 생성
+  movingItem.top = 0;
+  movingItem.left = 3;
+  movingItem.direction = 0;
+  tempMovingItem = { ...movingItem }; // 초기화 후 새로운 블럭 생성
+  renderBlocks();
+}
+
+function renderBlocks(moveType = '') {
+  if (!started) return;
+
+  // 구조 분해 할당
+  // tempMovingItem의 각 속성들을 개별적으로 변수에 할당
+  const { type, direction, top, left } = tempMovingItem;
+  const movingBlocks = document.querySelectorAll('.moving');
+  movingBlocks.forEach(moving => moving.classList.remove(type, 'moving'));
+
+  BLOCKS[type][direction].some(block => {
+    const x = block[0] + left; // 좌표 이동
+    const y = block[1] + top;
+    const target = tetrisBoard.childNodes[y] ? tetrisBoard.childNodes[y].childNodes[x] : null;
+    const isAvailable = checkEmpty(target);
+    if (isAvailable) target.classList.add(type, 'moving');
+    else {
+      tempMovingItem = { ...movingItem }; // tempMovingItem 원상 복구
+
+      if (moveType === 'replay') {
+        clearInterval(downInterval);
+        gameOver();
+      }
+      // call stack maximum 오류 방지를 위해 setTimeout 함수 사용
+      // event loop 내의 모든 event들이 실행된 후에 call stack으로 넣어줌
+      setTimeout(() => {
+        renderBlocks('replay');
+        if (moveType === 'top') seizeBlock(); // 블럭이 바닥에 닿았을 때 고정시킴
+      }, 0); // 재귀 함수
+      return true; // 쓸데 없는 반복문 제어
+    }
+  });
+
+  // 블럭이 영역 밖으로 벗어나지 않도록 방지
+  movingItem.left = left;
+  movingItem.top = top;
+  movingItem.direction = direction;
+}
